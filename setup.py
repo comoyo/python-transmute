11e2361530935c49ce1b3c401e6715a2e7b82e27
João Abecasis <joao@comoyo.com>
2014-01-20 22:51:21 +0100

    Initial commit
    This includes basic project information and  minimal directory
    structure.
    
    Unless otherwise noted code herein is licensed under Apache License 2.0,
    Copyright 2014 Telenor Digital AS.

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+execfile('transmute/version.py') # defines __version__
+
+project_metadata = {
+    'name':         'transmute',
+    'version':      __version__,
+    'author':       'João Abecasis',
+    'author_email': 'joao@comoyo.com',
+    'url':          'https://github.com/comoyo/python-transmute',
+    'description':  'Automatically update Python Eggs on application startup.',
+    'classifiers':  [
+                        "Programming Language :: Python",
+                        "License :: OSI Approved :: Apache Software License",
+                        "Operating System :: OS Independent",
+                        "Development Status :: 3 - Alpha",
+                        "Intended Audience :: Developers",
+                        "Topic :: Software Development :: Libraries :: Python Modules"
+                        "Topic :: System :: Installation/Setup",
+                        "Topic :: System :: Software Distribution",
+                    ],
+    'packages':     find_packages(),
+}
+setup(**project_metadata)
---
+++ transmute/version.py
@@ -0,0 +1,2 @@
+# No dependencies. File directly imported by top-level setup.py.
+__version__ = "1"

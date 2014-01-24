61e115e0ece5368fc1b2227aedc0de5bcb5d2f3e
João Abecasis <joao@comoyo.com>
2014-01-25 00:16:12 +0100

    Make bootstrap script (re)usable
    This breaks up bootstrap_transmute() and its inner functions into
    re-usable components. It also triggered a closer look at the existing
    code, and fixing of multiple... ahem... shortcomings:
    
      - local (cached) files are assumed to be good;
      - to that end, downloaded packages are given a temporary name and only
        renamed to usable name after the MD5 hash has been verified;
      - MD5 hashing will now check beyond the first 16K -- not that this was
        a concern for transmute itself ;-)
      - locally cached packages are usable, even if no network connection is
        available to "find" their names in PyPI...
    
    All code cleaned up, documentation added and brushed up.
    
    There are now three main purposes for the transmute.bootstrap module: it
    can be used stand-alone to bootstrap the transmute package; as well as
    to bootstrap independent packages and applications from PyPI; its
    functionality is otherwise made available to the rest of the transmute
    package.
    
    There is now some overlap between what bootstrap provides and what's in
    the rest of the package. This will be cleared in subsequent commits.

--- transmute/bootstrap.py
+++ transmute/bootstrap.py
@@ -14,115 +14,320 @@
 #   License for the specific language governing permissions and limitations
 #   under the License.
 
-# This script can be used to bootstrap installation of an application that uses
-# transmute. It can also be used to bootstrap installation and update of the
-# transmute module itself.
-#
-# To bootstrap an application, fill in the main function below to request needed
-# packages from transmute and launch the application.
-#
-# transmute packages are queried/fetched from PyPI and imported prior to calling
-# main(). Return values from main() are passed along to sys.exit().
+"""Bootstrap installation of transmute package.
+
+When imported (or directly executed), this module will make the latest transmute
+package available, downloading it from PyPI as needed. Dowloaded packages are
+stored in a local cache and will be reused in subsequent runs.
+
+This can also be used to bootstrap applications that use the transmute package.
+In this case, the module becomes the application's main executable. A couple of
+customization points are provided to support this use case with minimal effort:
+
+    requirements: global variable listing packages to be fetched from PyPI.
+    main(): placeholder for application specific logic. If the module is
+        executed as __main__ script, this gets called after packages in
+        requirements have been updated and added to sys.path.
+
+Additionally, bootstrap_starting(), bootstrap_succeeded() and bootstrap_failed()
+are called at specific points in the bootstrapping process.
+
+The following example could be used to load latest 'foobar' from PyPI and invoke
+the foobar.cli.main() entry point:
+
+    requirements = [ 'foobar' ]
+    def main():
+        import foobar.cli
+        return foobar.cli.main()
+
+"""
+
+
+################################################################################
+# Customization points to support running module as main application script.
+################################################################################
+
+requirements = [ 'transmute' ]
 
 def main():
-    ### Your code here?
-    pass
+    """Called when module is '__main__', after successful bootstrap."""
+    ### ~~~ Your code here ~~~ ###
 
-# Transmute bootstrap code follows.
-def bootstrap_transmute():
-    import os
-    import os.path
-    import sys
-    import urllib
+def bootstrap_starting():
+    """Called before attempting to load/download packages."""
 
-    pypi_url = 'https://pypi.python.org/pypi'
-    pypi_cache = os.path.expanduser('~/.python-transmute/cache/'
-            + urllib.quote(pypi_url, ''))
+def bootstrap_succeeded():
+    """Called after (updated) packages have been added to sys.path."""
 
-    os.path.isdir(pypi_cache) \
-            or os.makedirs(pypi_cache)
+def bootstrap_failed():
+    """Called when bootstrap fails to download packages from PyPI or load them
+    from the local cache.
+    """
+    raise RuntimeError("Unable to load 'transmute' package.")
 
-    def md5(path):
-        import hashlib
 
-        h = hashlib.md5()
-        try:
-            with open(path) as f:
-                h.update(f.read(16 * 1024))
-        except: pass
+################################################################################
+# transmute.bootstrap code follows.
+################################################################################
 
-        return h.hexdigest()
+import os
+import os.path
+import sys
 
-    def download_to(url, filename):
+def _chunk_read(file, chunk_size=16*1024):
+    """Read file one chunk at a time."""
+
+    while True:
+        chunk = file.read(chunk_size)
+        if chunk == '':
+            break
+        yield chunk
+
+def _md5(filename):
+    """Compute MD5 hash of a file."""
+
+    import hashlib
+
+    h = hashlib.md5()
+    try:
+        with open(filename) as file:
+            for chunk in _chunk_read(file):
+                h.update(chunk)
+    except: pass
+
+    return h.hexdigest()
+
+def _copy(source, destination, chunk_size):
+    """Read source into destination, one chunk at a time."""
+
+    for chunk in _chunk_read(source, chunk_size):
+        destination.write(chunk)
+
+def _download(url, filename, md5sum):
+    """Download url and save to filename, verify MD5 hash of content.
+
+    Content is initially saved to a temporary file and the content's MD5 hash is
+    verified before the file is atomically renamed to its desired filename.
+    """
+    import contextlib
+    import tempfile
+    import urllib2
+
+    dirname = os.path.dirname(filename)
+    dst = tempfile.NamedTemporaryFile(suffix='.download', dir=dirname)
+    with contextlib.closing(dst):
+        with contextlib.closing(urllib2.urlopen(url)) as src:
+            _copy(src, dst, 4 * 1024)
+        dst.flush()
+
+        if _md5(dst.name) != md5sum:
+            raise RuntimeError("MD5 hash of local file does not match "
+                    "expected value, upon downloading %s" % url)
+
+        # Depend on non-documented implementation of NamedTemporaryFile, a
+        # shame, but prettier than the alternative
+        dst.delete = False
+
+        os.rename(dst.name, filename)
+
+def require(baskets, *requirements):
+    """Satisfy requirements from given baskets."""
+
+    import pkg_resources
+
+    requirements = list(pkg_resources.parse_requirements(requirements))
+
+    environment = pkg_resources.Environment([])
+    for basket in baskets:
+        basket.fill_environment(environment, requirements)
+
+    working_set = pkg_resources.WorkingSet([])
+    for dist in working_set.resolve(requirements, env=environment):
+        dist._transmute_basket.make_local(dist)
+        working_set.add(dist)
+
+    for entry in sys.path:
+        if entry not in working_set.entries:
+            working_set.add_entry(entry)
+
+    sys.path[:] = working_set.entries
+
+
+class Basket(object):
+    """A container for Python Eggs."""
+
+    _cache_dir = os.path.expanduser('~/.python-transmute/cache')
+
+    def __init__(self, path=None, url=None):
+        assert (path is None) != (url is None)
+
+        if path is None:
+            path = self._prepare_cache(url)
+
+        self.path = os.path.join(path, '') # Keep trailing separator!
+        self.distributions = {}
+
+        self.add(*os.listdir(self.path))
+
+    def _prepare_cache(self, url):
+        import urllib
+
+        path = os.path.join(self._cache_dir, urllib.quote(url, ''))
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        return path
+
+    @classmethod
+    def _is_egg(cls, filename):
+        return filename[-4] == '.' \
+                and filename[-3:].lower() == 'egg'
+
+    def get_distribution(self, egg, **metadata):
+        from pkg_resources import Distribution
+
+        dist = Distribution.from_location(self.path + egg, egg)
+        dist._transmute_basket = self
+        dist._transmute_metadata = metadata
+
+        return dist
+
+    def add(self, *eggs, **metadata):
+        for egg in eggs:
+            if not self._is_egg(egg):
+                continue
+            self.distributions[egg] = self.get_distribution(egg, **metadata)
+
+    def fill_environment(self, environment, requirements=None):
+        for dist in self.distributions.itervalues():
+            environment.add(dist)
+
+    def fetch(self, dist, **metadata):
+        pass
+
+    def make_local(self, dist):
+        if not os.path.isfile(dist.location):
+            self.fetch(dist, **dist._transmute_metadata)
+
+
+class PyPIBasket(Basket):
+    """A proxy basket for eggs available in PyPI."""
+
+    _pypi_url = 'https://pypi.python.org/pypi'
+    _distributions = {}
+
+    def __init__(self):
+        super(PyPIBasket, self).__init__(url=self._pypi_url)
+
+    def fetch(self, dist, **metadata):
+        _download(metadata['url'], dist.location, metadata['md5_digest'])
+
+    def _add_packages(self, project_name, environment):
         import contextlib
-        import tempfile
+        import json
         import urllib2
 
-        dirname = os.path.dirname(filename)
-        try:
-            dst = tempfile.NamedTemporaryFile(dir=dirname, delete=False)
-            src = urllib2.urlopen(url)
-            while True:
-                chunk = src.read(4 * 1024)
-                if not chunk:
-                    break
-                dst.write(chunk)
-        except:
-            if dst:
-                dst.close() # Damn you, Windows!
-                os.remove(dst.name)
-        else:
-            os.rename(dst.name, filename)
-        finally:
-            if src: src.close()
-            if dst: dst.close()
+        if project_name not in self._distributions:
+            url = '%s/%s/json' % (self._pypi_url, project_name)
+            with contextlib.closing(urllib2.urlopen(url)) as req:
+                metadata = json.load(req)
 
-    def get_package_info(package_name):
-        import json, urllib2
+            distributions = []
+            for package in metadata['urls']:
+                if not sys.version.startswith(package['python_version']) \
+                        or package['packagetype'] != 'bdist_egg':
+                    continue
+                distributions.append(
+                        self.get_distribution(package['filename'], **package))
 
-        url = '%s/%s/json' % (pypi_url, package_name)
-        req = urllib2.urlopen(url)
-        try:
-            metadata = json.load(req)
-        finally:
-            req.close()
+            self._distributions[project_name] = distributions
 
-        for info in metadata['urls']:
-            if sys.version.startswith(info['python_version']) \
-                    and info['packagetype'] == 'bdist_egg':
-                return info['url'], info['filename'], info['md5_digest']
+        for dist in self._distributions[project_name]:
+            environment.add(dist)
 
-        raise RuntimeError(
-                "No suitable packages for '%s' in %s" % (package_name, url))
+    def fill_environment(self, environment, requirements=None):
+        super(PyPIBasket, self).fill_environment(environment, requirements)
 
-    def get_package(package):
-        url, filename, md5sum = get_package_info(package)
-        filename = os.path.join(pypi_cache, filename)
-        if md5(filename) == md5sum:
-            return filename
+        for req in requirements:
+            try: self._add_packages(req.project_name, environment)
+            except: raise
 
-        download_to(url, filename)
-        if md5(filename) == md5sum:
-            return filename
 
-        raise RuntimeError('Unable to download package from %s' % url)
+def bootstrap():
+    """Bootstrap 'transmute' package, making it available for import.
 
-    package = get_package('transmute')
-    sys.path.insert(0, package)
+    Actual packages to be bootstrapped are defined in the global variable
+    requirements.
 
-    if 'transmute' in sys.modules:
-        reload(sys.modules['transmute'])
-    else:
-        import transmute
+    Latest packages are downloaded from PyPI, if available, and added to
+    sys.path.
+    """
+    bootstrap_starting()
+    try: # Fetch latest packages from PyPI
+        require([ PyPIBasket() ], requirements)
+        bootstrap_succeeded()
+        return
+    except: pass
 
-if __name__ == 'transmute':
-    bootstrap_transmute()
-else:
-    try:
-        import transmute
-    except ImportError:
-        bootstrap_transmute()
+    try: # Use previously downloaded packages, if download fails
+        require([ Basket(url=PyPIBasket._pypi_url) ], requirements)
+        bootstrap_succeeded()
+        return
+    except: pass
 
-if __name__ == '__main__':
+    bootstrap_failed()
+
+def _clean_namespace():
+    """Clean module's namespace.
+
+    Used prior to a reload() of the module when the present module is used to
+    bootstrap the Real Thing (tm).
+    """
+    global __doc__, os, sys
+    del __doc__
+    del os
+    del sys
+
+    global requirements, main, bootstrap_starting, bootstrap_succeeded, \
+            bootstrap_failed
+    del requirements
+    del main
+    del bootstrap_starting
+    del bootstrap_succeeded
+    del bootstrap_failed
+
+    global _chunk_read, _md5, _copy, _download
+    del _chunk_read
+    del _md5
+    del _copy
+    del _download
+
+    global require, Basket, PyPIBasket
+    del require
+    del Basket
+    del PyPIBasket
+
+    global bootstrap, _clean_namespace
+    del bootstrap
+    del _clean_namespace
+
+
+if __name__ in requirements:
+    bootstrap()
+
+    # Module is filling in for a package so cleanup namespace and reload
+    _clean_namespace()
+
     import sys
+    reload(sys.modules[__name__])
+else:
+    import importlib
+    try:
+        for package in requirements:
+            importlib.import_module(package)
+    except ImportError:
+        bootstrap()
+
+
+# Running as self-contained script, invoke user hook.
+if __name__ == '__main__':
     sys.exit(main())

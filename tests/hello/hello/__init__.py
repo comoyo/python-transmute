9f76a0a673b83b2de6a5f8a37b0628796da4a88c
João Abecasis <joao@comoyo.com>
2014-02-14 01:40:50 +0100

    Allow 'hello' test package to be used unpackaged

--- tests/hello/hello/__init__.py
+++ tests/hello/hello/__init__.py
@@ -1,6 +1,7 @@
 import pkg_resources
 
-version = pkg_resources.require(__name__)[0].version
+try: version = pkg_resources.require(__name__)[0].version
+except: version = 'unknown'
 
 def greet(name='World'):
     print "Hi, %s! -- from version %s of %s" % (name, version, __name__)
